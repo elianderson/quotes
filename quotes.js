00706034a037116d7f0b97c8f01ac672621ea366
Michael Jones <mjfreshyfresh@MJs.local>
2010-10-10 12:18:37 -0700

    added a few JSON quotes

---
+++ quotes.js
@@ -0,0 +1,24 @@
+{
+    "quotes": [
+        {
+            "quote": "The problem with guessing games is you win a bunch of candy corn, and who the hell wants that.",
+            "by": "Bornstein"
+        },
+        {
+            "quote": "You can teach a man to fish, Or you can give him a moldy fish.",
+            "by": "Bornstein"
+        },
+        {
+            "quote": "No amount of graph paper will save you.",
+            "by": "Bornstein"
+        },
+        {
+            "quote": "Clear your cache, kill an animal... do whatever it takes.",
+            "by": "Bornstein"
+        },
+        {
+            "quote": "That code should be thrown on the floor, set on fire, and jumped up and down on!",
+            "by": "Bornstein"
+        } 
+    ]
+}
